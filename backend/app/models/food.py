7051c22d9a183159bd123d6f93a626aad9eaae3f
a-violeta <iulia.violeta.avram@gmail.com>
2026-03-28 16:20:36 +0200

    File structure, docker, basic workflow and installation instructions

---
+++ backend/app/models/food.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Float
+from app.db import Base
+
+class Food(Base):
+    __tablename__ = "foods"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    calories = Column(Integer)
+    protein = Column(Float)
+    carbs = Column(Float)
+    fat = Column(Float)
+    fiber = Column(Float)
+    sodium = Column(Float)
